08945b09cb05362195b0096ffddd100ba1e1b8bf
scottsbluffrobotics2025-jpg <scottsbluffrobotics2025@gmail.com>
2025-11-18 16:41:59 -0700

    Update Example.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Example.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Example.java
@@ -2,18 +2,35 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class Example extends OpMode {
 
+    DcMotor testMotor;
+    Servo servo;
+    DcMotor anything;
+    double servoPos;
+
     public void init() {
-        if (gamepad1.b) {
-            telemetry.addData("B is pressed","yeah");
-        }
+        // this is how you log telemetry
+        telemetry.addData("This is the caption input","this is the value");
+
+        // This is how you initialize the motor and servo
+        testMotor = hardwareMap.get(DcMotor.class, "testMotor");
+        servo = hardwareMap.get(Servo.class, "testServo");
+        anything = hardwareMap.get(DcMotor.class, "testmoter2");
     }
+
+
 
     public void loop() {
+        servoPos = gamepad1.right_trigger-gamepad1.left_trigger;
 
+        servo.setPosition(servoPos);
+        testMotor.setPower(gamepad1.right_stick_x);
     }
+
 
 }
